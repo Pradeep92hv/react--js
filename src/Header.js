9ccbffacdfbe0e05c925d629a7b76abbceb31291
Pradeep  H V <hvpradeep92@gmail.com>
2024-07-02 18:04:21 +0530

    appying style

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
-
 const Header = () => {
-  return (
-   <header>
-    <h1>Groceries list</h1>
-   </header>
-  )
-}
-
-export default Header
+    const headerStyle = {
+      backgroundColor: 'mediumblue',
+      color: '#fff'
+    }
+  
+    return (
+      <header style={headerStyle}>
+        <h1>Groceries List</h1>
+      </header>
+    )
+  }
+  
+  export default Header
+  
