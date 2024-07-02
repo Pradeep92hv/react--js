e456e7e3a3e1caf5195537ff730c0551bd85c976
Pradeep  H V <hvpradeep92@gmail.com>
2024-07-02 17:53:47 +0530

    36min chapter3,  functional componenets

---
+++ src/Content.js
@@ -0,0 +1,14 @@
+
+const Content = () => {
+
+    const name="pradeep"
+  return (
+   <main>
+<p>
+    Welcome {name}!
+</p>
+   </main>
+  )
+}
+
+export default Content
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+   <header>
+    <h1>Groceries list</h1>
+   </header>
+  )
+}
+
+export default Header
