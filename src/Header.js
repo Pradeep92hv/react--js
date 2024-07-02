1774139ed543dd9823064acc045ca990b4462aaf
Pradeep  H V <hvpradeep92@gmail.com>
2024-07-02 18:13:57 +0530

    chapet 5  appylied style

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,7 @@
 const Header = () => {
-    const headerStyle = {
-      backgroundColor: 'mediumblue',
-      color: '#fff'
-    }
-  
+    
     return (
-      <header style={headerStyle}>
+      <header >
         <h1>Groceries List</h1>
       </header>
     )
