5eb088687ad3a370c1dc265f769235c344876f9f
Pradeep  H V <hvpradeep92@gmail.com>
2024-07-02 18:31:35 +0530

    51min chapet 5 click event

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,37 @@
-
 const Content = () => {
+    const handleNameChange = () => {
+        const names = ['Pradeep', 'Yanjare', 'Gowda'];
+        const int = Math.floor(Math.random() * 3);
+        return names[int];
+    }
 
-    const name="pradeep"
-  return (
-   <main>
-<p>
-    Welcome {name}!
-</p>
-   </main>
-  )
+    const handleclick= ( ) =>{
+        console.log("clicked")
+    }
+
+    const handleclick1= (name) =>{
+        console.log(`clicked by ${name}`)
+    }
+
+    const handleclick2= (e) =>{
+        console.log(e.target.innerText)
+    }
+
+
+    return (
+        <main>
+            <p onClick={(e)=> handleclick2(e)}>
+                Hello {handleNameChange()}!
+            </p>
+
+            <button onClick={handleclick}>click here</button>
+
+            <button onClick={()=> handleclick1("pradeephv")}>click here</button>
+
+            <button onClick={(e)=> handleclick2(e)}>click here</button>
+          
+        </main>
+    )
 }
 
 export default Content
