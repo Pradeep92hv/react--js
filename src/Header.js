c361c49aba484b4c04674f3db32b4161b4b8f723
Pradeep  H V <hvpradeep92@gmail.com>
2024-07-03 12:32:25 +0530

    1.49hrs chapter 8.1 props drillings

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
-const Header = () => {
+const Header = (props) => {
     
     return (
       <header >
-        <h1>Groceries List</h1>
+        <h1 > {props.title_name}</h1>
       </header>
     )
   }
-  
+  Header.defaultProps={
+    title_name : "Default name"
+  }
   export default Header
   
